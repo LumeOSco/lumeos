1dfc5a6d4f8227811efbb818d569e4c29cd4325c
LumeOSco <166047286+LumeOSco@users.noreply.github.com>
2024-04-08 22:26:00 +0200

    Update admin.js

--- admin.js
+++ admin.js
@@ -1,14 +1,16 @@
-// JavaScript for admin login functionality
-
 document.addEventListener('DOMContentLoaded', function () {
     const form = document.getElementById('adminForm');
     form.addEventListener('submit', function (event) {
         event.preventDefault();
         const password = document.getElementById('password').value;
         if (password === '1212') {
-            window.location.href = 'admin-panel.html'; // Redirect to admin panel
+            shutdownWebsite(); // Shutdown the website
         } else {
             alert('Incorrect password. Access denied.');
         }
     });
 });
+
+function shutdownWebsite() {
+    document.querySelector('body').innerHTML = '<h1>Website is currently down for maintenance.</h1>';
+}
