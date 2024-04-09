af22a04006029ef51c86ddc25b6098334edbd07e
LumeOSco <166047286+LumeOSco@users.noreply.github.com>
2024-04-09 12:12:22 +0200

    Delete main.js

--- main.js
+++
@@ -1,8 +0,0 @@
-// JavaScript for animations
-
-document.addEventListener('DOMContentLoaded', function () {
-    const animationTargets = document.querySelectorAll('.animation-target');
-    animationTargets.forEach(target => {
-        target.classList.add('animate-in');
-    });
-});
